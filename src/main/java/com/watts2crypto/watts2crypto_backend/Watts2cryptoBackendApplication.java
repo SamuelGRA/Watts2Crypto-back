e995df547c5700b0590a7ba6fd759b11bc7f347d
SamuelGRA <samuelgranadooliva36@gmail.com>
2026-03-09 02:23:59 +0100

    First commit

---
+++ src/main/java/com/watts2crypto/watts2crypto_backend/Watts2cryptoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.watts2crypto.watts2crypto_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Watts2cryptoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Watts2cryptoBackendApplication.class, args);
+	}
+
+}
